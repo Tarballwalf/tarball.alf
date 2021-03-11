689616d32ae7825cdbfe096d155b8f2c082c4dea
Tarballwalf <nicu.pilt@gmail.com>
2021-03-11 16:15:18 +0200

    new project

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
 setInterval(() => {
     document.getElementById('time2').innerHTML = moment.tz('Europe/Bucharest').format('dddd, MMMM Do   HH:mm (hh:mm A) z')
 }, 300)
+
+var toggler = document.getElementsByClassName("caret");
+          var i;
+          
+          for (i = 0; i < toggler.length; i++) {
+            toggler[i].addEventListener("click", function() {
+              this.parentElement.querySelector(".nested").classList.toggle("active");
+              this.classList.toggle("caret-down");
+            });
+          }
